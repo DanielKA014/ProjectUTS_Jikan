fa788f3857e7e6a7464e3523e8e09da42ac89d6d
Seanstanislaus <134819954+Seanstanislaus@users.noreply.github.com>
2025-04-24 13:40:57 +0700

    getAnimeStreaming,   getCharacterFullById,   getCharacterById, };
     //mendapatkan info terkait platform anime streaming
      route.get('/:id/streaming', animeController.getAnimeStreaming);
    
      // mendapatkan karakter full berdasarkan Id karakter dalam anime
      route.get('/:animeId/characters/:characterId/full', animeController.getCharacterFullById);
    
      // mendapatkan karakter berdasarkan Id anime
      route.get('/characters/:characterId', animeController.getCharacterById);
    };

--- src/api/components/animes/animes-repository.js
+++ src/api/components/animes/animes-repository.js
@@ -82,7 +82,11 @@ async function getCharacterFullById(animeId) {
 
 async function getCharacterById(animeId) {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   return Characters.getCharacterFullById(animeId); 
+=======
+  return Characters.getCharacterFullById(animeId); 
+>>>>>>> Stashed changes
 =======
   return Characters.getCharacterFullById(animeId); 
 >>>>>>> Stashed changes
