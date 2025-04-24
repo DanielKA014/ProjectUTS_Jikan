49d7a2976965276852fce5f151cde8dc87e08ed9
Jonathan-Sanjaya <jonathan.535240058@stu.untar.ac.id>
2025-04-24 14:42:15 +0700

    fix error

--- src/api/components/characters/anime-characters-route.js
+++ src/api/components/characters/anime-characters-route.js
@@ -6,9 +6,6 @@ const characterController = require('./characters-controller');
 module.exports = (app) => {
   app.use('/anime/:id/characters', route);
 
-  // Add character to anime
-  route.post('/', characterController.createCharacter);
-
   // Get anime characters
   route.get('/', characterController.getCharactersByAnimeId);
 
